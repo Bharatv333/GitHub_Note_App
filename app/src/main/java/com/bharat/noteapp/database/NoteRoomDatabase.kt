00ebb842ff4afa27ccbee5c4548007c7a616113d
Bharatv333 <varmabharath58@gmail.com>
2022-11-28 20:40:33 +0530

    This is my First Commit

---
+++ app/src/main/java/com/bharat/noteapp/database/NoteRoomDatabase.kt
@@ -0,0 +1,38 @@
+package com.bharat.noteapp.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.bharat.noteapp.entity.Note
+import com.bharat.noteapp.dao.NoteDao
+
+//Database annotation to specify the entities and set version
+@Database(entities = [Note::class], version = 1, exportSchema = false)
+abstract class NoteRoomDatabase : RoomDatabase() {
+
+    companion object {
+        @Volatile
+        private var INSTANCE: NoteRoomDatabase? = null
+
+        fun getDatabase(context: Context): NoteRoomDatabase {
+            return INSTANCE
+                ?: synchronized(this) {
+                    // Create database here
+                    val instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        NoteRoomDatabase::class.java,
+                        "note_db"
+                    )
+                        .allowMainThreadQueries() //allows Room to executing task in main thread
+                        .fallbackToDestructiveMigration() //allows Room to recreate database if no migrations found
+                        .build()
+
+                    INSTANCE = instance
+                    instance
+                }
+        }
+    }
+
+    abstract fun getNoteDao(): NoteDao
+}
